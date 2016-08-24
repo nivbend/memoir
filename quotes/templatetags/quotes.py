44dcc9aa8d70ac8fabe80043a21799df9073d733
Niv Ben-David <nivbend@gmail.com>
2016-08-24 22:56:56 +0300

    Split the single quote template into tags
    This allows us to move the single_quote template itself to the common templates directory.
    Also, slightly modified the way the template decides whether or not to link to the quote page.

---
+++ quotes/templatetags/quotes.py
@@ -0,0 +1,44 @@
+from django.core.urlresolvers import reverse
+from django.template import Library
+
+register = Library()
+
+@register.inclusion_tag('quotes/quote/user_buttons.html', takes_context = True)
+def user_buttons(context):
+    quote = context['quote']
+
+    return {
+        'edit_url': reverse('quote:edit', kwargs = {'pk': quote.pk, }),
+        'delete_modal_id': 'delete_modal_%d' % (quote.pk, ),
+    }
+
+@register.inclusion_tag('quotes/quote/title.html', takes_context = True)
+def quote_title(context):
+    quote = context['quote']
+    with_link = context.get('with_link', True)
+
+    detail_url = ''
+    if with_link:
+        detail_url = reverse('quote:detail', kwargs = {'pk': quote.pk, })
+
+    return {
+        'quote_title': str(quote),
+        'detail_url': detail_url,
+    }
+
+@register.inclusion_tag('quotes/quote/author.html', takes_context = True)
+def quote_author(context):
+    return {'author': context['quote'].author.username, }
+
+@register.inclusion_tag('quotes/quote/body.html', takes_context = True)
+def quote_body(context):
+    return {'quote_text': context['quote'].text, }
+
+@register.inclusion_tag('quotes/quote/delete_modal.html', takes_context = True)
+def delete_modal(context):
+    quote = context['quote']
+
+    return {
+        'delete_modal_id': 'delete_modal_%d' % (quote.id, ),
+        'delete_url': reverse('quote:delete', kwargs = {'pk': quote.pk, }),
+    }
