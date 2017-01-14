e6bb95f8540c34fc41b8171914b3901bcf1c9a9b
Niv Ben-David <nivbend@gmail.com>
2017-01-14 12:55:09 +0200

    Force at least two words in text (fix #4)

--- quotes/models.py
+++ quotes/models.py
@@ -1,11 +1,31 @@
 from __future__ import unicode_literals
 from django.utils.encoding import python_2_unicode_compatible
+from django.utils.deconstruct import deconstructible
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 from django.db.models import Model, SET_NULL
 from django.db.models import DateTimeField, ForeignKey, CharField, TextField, ManyToManyField
 from .regex import REGEX_SPEAKER, REGEX_REFERENCE
+
+@deconstructible
+class WordCountValidator(object):
+    def __init__(self, min_count = 0, max_count = None):
+        if max_count is not None and max_count < min_count:
+            raise ValueError('min_count must be less than max_count')
+
+        self.min_count = min_count
+        self.max_count = max_count
+
+    def __call__(self, value):
+        word_count = sum(len(line.split(' ')) for line in value.splitlines())
+
+        if word_count < self.min_count:
+            raise ValidationError('Enter at least %d words' % (self.min_count, ))
+
+        if self.max_count and self.max_count < word_count:
+            raise ValidationError('Can\'t enter more than %d words' % (self.max_count, ))
 
 @python_2_unicode_compatible
 class Quote(Model):
@@ -13,7 +33,7 @@ class Quote(Model):
     last_modified = DateTimeField(auto_now = True)
     author = ForeignKey(settings.AUTH_USER_MODEL, related_name = 'author_of')
     title = CharField(max_length = 120, blank = True)
-    text = TextField()
+    text = TextField(validators = [WordCountValidator(min_count = 2), ])
     mentions = ManyToManyField(
         settings.AUTH_USER_MODEL,
         blank = True,
---
+++ quotes/test/test_quotes.py
@@ -0,0 +1,37 @@
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from django.test import TestCase
+from quotes.models import Quote
+
+class TestQuotes(TestCase):
+    def setUp(self):
+        self.silent_bob = User.objects.create_user(
+            username = 'sbob',
+            password = 'password')
+
+    def test_empty_quote(self):
+        with self.assertRaises(ValidationError):
+            self.create_quote('')
+
+    def test_single_word_text(self):
+        with self.assertRaises(ValidationError):
+            self.create_quote('*shrugs*')
+
+    def test_valid_text(self):
+        self.create_quote('Shut up.')
+
+    def test_edit_text(self):
+        quote = self.create_quote('Do something.')
+
+        quote.text = '*nods*'
+        with self.assertRaises(ValidationError):
+            quote.full_clean()
+
+        quote.text = ''
+        with self.assertRaises(ValidationError):
+            quote.full_clean()
+
+    def create_quote(self, text):
+        quote = Quote.objects.create(author = self.silent_bob, text = text)
+        quote.full_clean()
+        return quote
