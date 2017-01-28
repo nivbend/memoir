5b0d3042db61e034ac9170bc8031d0fa8222de31
Niv Ben-David <nivbend@gmail.com>
2017-01-28 18:08:47 +0200

    Add mentions autocomplete to textareas (close #3)

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,8 +1,9 @@
 from __future__ import unicode_literals
 from django.conf.urls import url
 
-from .views import ProfileView
+from .views import ProfileView, lookup_view
 
 urlpatterns = [
+    url(r'^lookup$', lookup_view, name = 'user-search'),
     url(r'^(?P<username>\w+)$', ProfileView.as_view(), name = 'user-profile'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,10 @@
 from __future__ import unicode_literals
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
+from django.http import JsonResponse
+from django.db.models import Q
 from django.contrib.auth import get_user_model
-from django.views.generic import DetailView
+from django.views.generic import View, DetailView
 from .models import UserProfile
 
 @method_decorator(login_required, name = 'dispatch')
@@ -19,3 +21,24 @@ class ProfileView(DetailView):
     def get_object(self, queryset = None):
         # Pass the profile object to the template, not the User.
         return super(ProfileView, self).get_object(queryset).profile
+
+def lookup_view(request):
+    name = request.GET['q']
+
+    query = Q()
+    for word in name.split(' '):
+        if not word.strip():
+            continue
+
+        query |= Q(first_name__icontains = word)
+        query |= Q(last_name__icontains = word)
+
+    users = get_user_model().objects.filter(query)
+
+    return JsonResponse({
+        'matches': [{
+                'username': u.username,
+                'full_name': u.get_full_name(),
+            }
+            for u in users],
+    })
