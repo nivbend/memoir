27babda5d426cb0c9f65d359f6cf412cb8e4290e
Niv Ben-David <nivbend@gmail.com>
2017-01-28 18:34:59 +0200

    Register Comment in admin panel

--- quotes/admin.py
+++ quotes/admin.py
@@ -1,7 +1,11 @@
 from __future__ import unicode_literals
 from django.contrib.admin import ModelAdmin, register
-from .models import Quote
+from .models import Quote, Comment
 
 @register(Quote)
 class QuoteAdmin(ModelAdmin):
+    pass
+
+@register(Comment)
+class CommentAdmin(ModelAdmin):
     pass
