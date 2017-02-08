8a535319c91ca780d1cbaac9414eb0348414b544
Niv Ben-David <nivbend@gmail.com>
2017-02-08 23:07:41 +0200

    Exclude periods, commas and semi-colons in nicknames

--- quotes/regex.py
+++ quotes/regex.py
@@ -3,8 +3,8 @@ import re
 
 REGEX_SPEAKER = re.compile(
     r''.join([
-        r'^([\w_]+)',                   # Username.
-        r'(?:\|([^":\s]+|"[^":]+"))?',  # Forced nickname.
+        r'^([\w_]+)',                       # Username.
+        r'(?:\|([^"\s,.;:]+|"[^":]+"))?',   # Forced nickname.
         r':',
     ]),
     re.MULTILINE)
@@ -13,6 +13,6 @@ REGEX_REFERENCE = re.compile(
     r''.join([
         r'(^|[^\\])@',                  # Mark reference.
         r'([\w_]+)\b',                  # Username.
-        r'(?:\|([^"\s]+|"[^"]+"))?',    # Forced nickname.
+        r'(?:\|([^"\s,.;]+|"[^"]+"))?', # Forced nickname.
     ]),
     re.MULTILINE)
